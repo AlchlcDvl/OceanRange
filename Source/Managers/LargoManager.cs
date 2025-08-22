6e9f5208cb1d789b4982ddbba3a0ebc53363178f
AmorphousSystem <anastas.millercsapo@outlook.com>
2025-08-22 15:27:00 +1000

    Combined CraftLargo, AddLargoEatMap, and LargoTweaks

--- Source/Managers/LargoManager.cs
+++ Source/Managers/LargoManager.cs
@@ -34,93 +34,62 @@ public static class LargoManager
     */
     public static readonly Dictionary<IdentifiableId, List<(IdentifiableId, IdentifiableId)>> LargoMaps = [];
 
-    public static void AddLargoEatMap(IdentifiableId largoId, IdentifiableId slimeId1, IdentifiableId slimeId2)
+    public static void CreateLargo(Identifiable.Id largoId, Identifiable.Id slime1, Identifiable.Id slime2, SlimeRegistry.LargoProps props, out SlimeDefinition largoDefinition, out SlimeAppearance largoAppearance, out GameObject largoObject, SlimeRegistry.LargoProps slime1SSProps = SlimeRegistry.LargoProps.NONE, SlimeRegistry.LargoProps slime2SSProps = SlimeRegistry.LargoProps.NONE, SlimeRegistry.LargoProps slime12SSProps = SlimeRegistry.LargoProps.NONE)
     {
-        if (!LargoMaps.TryGetValue(slimeId1, out var slime1Values))
-            slime1Values = LargoMaps[slimeId1] = [];
+        SlimeDefinition slime1Def = slime1.GetSlimeDefinition();
+        SlimeDefinition slime2Def = slime2.GetSlimeDefinition();
+        SlimeDefinition def = SlimeRegistry.CombineDefinitions(largoId, slime1Def, slime2Def, props);
+        SlimeAppearance slimeAppearance = SlimeRegistry.CombineAppearances(slime1Def.AppearancesDefault[0], slime2Def.AppearancesDefault[0], SlimeAppearance.AppearanceSaveSet.CLASSIC, props);
+        GameObject gameObject = SlimeRegistry.CombineSlimePrefabs(def);
+        TranslationPatcher.AddActorTranslation("l." + largoId.ToString().ToLower(), SlimeRegistry.GenerateLargoName(largoId));
 
-        if (!LargoMaps.TryGetValue(slimeId2, out var slime2Values))
-            slime2Values = LargoMaps[slimeId2] = [];
+        /*SRSingleton<GameContext>.Instance.DLCDirector.onPackageInstalled += delegate (DLCPackage.Id x)
+        {
+            if (x == Id.SECRET_STYLE)
+            {
+                SlimeAppearance appearanceForSet = slime1Def.GetAppearanceForSet(SlimeAppearance.AppearanceSaveSet.SECRET_STYLE);
+                SlimeAppearance appearanceForSet2 = slime2Def.GetAppearanceForSet(SlimeAppearance.AppearanceSaveSet.SECRET_STYLE);
+                if (appearanceForSet != null && appearanceForSet2 != null)
+                {
+                    RegisterAppearance(def, CombineAppearances(appearanceForSet, appearanceForSet2, SlimeAppearance.AppearanceSaveSet.SECRET_STYLE, (slime12SSProps == LargoProps.NONE) ? props : slime12SSProps));
+                    RegisterAppearance(def, CombineAppearances(slime1Def.AppearancesDefault[0], appearanceForSet2, SlimeAppearance.AppearanceSaveSet.SECRET_STYLE, (slime2SSProps == LargoProps.NONE) ? props : slime2SSProps));
+                    RegisterAppearance(def, CombineAppearances(appearanceForSet, slime2Def.AppearancesDefault[0], SlimeAppearance.AppearanceSaveSet.SECRET_STYLE, (slime1SSProps == LargoProps.NONE) ? props : slime1SSProps));
+                }
+                else if (appearanceForSet != null)
+                {
+                    RegisterAppearance(def, CombineAppearances(appearanceForSet, slime2Def.AppearancesDefault[0], SlimeAppearance.AppearanceSaveSet.SECRET_STYLE, (slime1SSProps == LargoProps.NONE) ? props : slime1SSProps));
+                }
+                else if (appearanceForSet2 != null)
+                {
+                    RegisterAppearance(def, CombineAppearances(slime1Def.AppearancesDefault[0], appearanceForSet2, SlimeAppearance.AppearanceSaveSet.SECRET_STYLE, (slime2SSProps == LargoProps.NONE) ? props : slime2SSProps));
+                }
+            }
+        };*/
 
-        slime1Values.Add((largoId, slimeId2));
-        slime2Values.Add((largoId, slimeId1));
-    }
+        def.AppearancesDefault = new SlimeAppearance[1] { slimeAppearance };
+        LookupRegistry.RegisterIdentifiablePrefab(gameObject);
+        SlimeRegistry.RegisterAppearance(def, slimeAppearance);
+        SlimeRegistry.RegisterSlimeDefinition(def);
+        largoDefinition = def;
+        largoAppearance = slimeAppearance;
+        largoObject = gameObject;
 
-    public static void LoadAllLargos()
-    {
-        //COCO LARGOS
-        /*SlimeRegistry.CraftLargo(Ids.PINK_COCO_LARGO, Ids.COCO_SLIME, IdentifiableId.PINK_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.PINK_COCO_LARGO, Ids.COCO_SLIME, IdentifiableId.PINK_SLIME);
+        if (!LargoMaps.TryGetValue(slime1, out var slime1Values))
+            slime1Values = LargoMaps[slime1] = [];
 
-        SlimeRegistry.CraftLargo(Ids.COCO_SABER_LARGO, Ids.COCO_SLIME, IdentifiableId.SABER_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_SABER_LARGO, Ids.COCO_SLIME, IdentifiableId.SABER_SLIME);
+        if (!LargoMaps.TryGetValue(slime2, out var slime2Values))
+            slime2Values = LargoMaps[slime2] = [];
 
-        SlimeRegistry.CraftLargo(Ids.COCO_QUANTUM_LARGO, Ids.COCO_SLIME, IdentifiableId.QUANTUM_SLIME, SlimeRegistry.LargoProps.REPLACE_BASE_MAT_AS_SLIME2 | SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_QUANTUM_LARGO, Ids.COCO_SLIME, IdentifiableId.QUANTUM_SLIME);
+        slime1Values.Add((largoId, slime2));
+        slime2Values.Add((largoId, slime1));
 
-        SlimeRegistry.CraftLargo(Ids.COCO_HONEY_LARGO, Ids.COCO_SLIME, IdentifiableId.HONEY_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_HONEY_LARGO, Ids.COCO_SLIME, IdentifiableId.HONEY_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_PHOSPHOR_LARGO, Ids.COCO_SLIME, IdentifiableId.PHOSPHOR_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_PHOSPHOR_LARGO, Ids.COCO_SLIME, IdentifiableId.PHOSPHOR_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_MOSAIC_LARGO, Ids.COCO_SLIME, IdentifiableId.MOSAIC_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_MOSAIC_LARGO, Ids.COCO_SLIME, IdentifiableId.MOSAIC_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_TANGLE_LARGO, Ids.COCO_SLIME, IdentifiableId.TANGLE_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_TANGLE_LARGO, Ids.COCO_SLIME, IdentifiableId.TANGLE_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_BOOM_LARGO, Ids.COCO_SLIME, IdentifiableId.BOOM_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_BOOM_LARGO, Ids.COCO_SLIME, IdentifiableId.BOOM_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_RAD_LARGO, Ids.COCO_SLIME, IdentifiableId.RAD_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_RAD_LARGO, Ids.COCO_SLIME, IdentifiableId.RAD_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_ROCK_LARGO, Ids.COCO_SLIME, IdentifiableId.ROCK_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_ROCK_LARGO, Ids.COCO_SLIME, IdentifiableId.ROCK_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_TABBY_LARGO, Ids.COCO_SLIME, IdentifiableId.TABBY_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_TABBY_LARGO, Ids.COCO_SLIME, IdentifiableId.TABBY_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_HUNTER_LARGO, Ids.COCO_SLIME, IdentifiableId.HUNTER_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_HUNTER_LARGO, Ids.COCO_SLIME, IdentifiableId.HUNTER_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_CRYSTAL_LARGO, Ids.COCO_SLIME, IdentifiableId.CRYSTAL_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_CRYSTAL_LARGO, Ids.COCO_SLIME, IdentifiableId.CRYSTAL_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_DERVISH_LARGO, Ids.COCO_SLIME, IdentifiableId.DERVISH_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_DERVISH_LARGO, Ids.COCO_SLIME, IdentifiableId.DERVISH_SLIME);
-
-        SlimeRegistry.CraftLargo(Ids.COCO_MESMER_LARGO, Ids.COCO_SLIME, Ids.MESMER_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.COCO_MESMER_LARGO, Ids.COCO_SLIME, Ids.MESMER_SLIME);*/
-
-        // SlimeRegistry.CraftLargo(Ids.COCO_HERMIT_LARGO, Ids.COCO_SLIME, Ids.HERMIT_SLIME, SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-
-        //MESMER LARGOS
-
-        //HERMIT LARGOS
-
-        //ROSI LARGOS
-        SlimeRegistry.CraftLargo(Ids.ROSI_MINE_LARGO, Ids.ROSI_SLIME, Ids.MINE_SLIME, SlimeRegistry.LargoProps.REPLACE_BASE_MAT_AS_SLIME2 | SlimeRegistry.LargoProps.RECOLOR_SLIME1_ADDON_MATS | SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES);
-        AddLargoEatMap(Ids.ROSI_MINE_LARGO, Ids.MINE_SLIME, Ids.ROSI_SLIME);
-        //material??
-        //fix Mine addons
-
-        SlimeRegistry.CraftLargo(Ids.ROSI_LANTERN_LARGO, Ids.ROSI_SLIME, Ids.LANTERN_SLIME, SlimeRegistry.LargoProps.RECOLOR_BASE_MAT_AS_SLIME2 | SlimeRegistry.LargoProps.RECOLOR_SLIME1_ADDON_MATS | SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES | SlimeRegistry.LargoProps.SWAP_EYES);
-        AddLargoEatMap(Ids.ROSI_LANTERN_LARGO, Ids.LANTERN_SLIME, Ids.ROSI_SLIME);
-
-        //MINE LARGOS
-        SlimeRegistry.CraftLargo(Ids.MINE_LANTERN_LARGO, Ids.MINE_SLIME, Ids.LANTERN_SLIME, SlimeRegistry.LargoProps.RECOLOR_SLIME2_ADDON_MATS | SlimeRegistry.LargoProps.GENERATE_NAME | SlimeRegistry.LargoProps.GENERATE_SECRET_STYLES | SlimeRegistry.LargoProps.SWAP_EYES);
-        AddLargoEatMap(Ids.MINE_LANTERN_LARGO, Ids.MINE_SLIME, Ids.LANTERN_SLIME);
-    }
-    public static void LargoTweaks()
-    {
-        SlimeAppearanceStructure[] rmStructures = SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(Ids.ROSI_MINE_LARGO).AppearancesDefault[0].Structures;
-        GameObject slimeRosi = SRSingleton<GameContext>.Instance.LookupDirector.GetIdentifiable(Ids.ROSI_SLIME).gameObject;
-        SlimeAppearanceStructure rmBitsStructure = Array.Find(rmStructures, x => x.Element.Prefabs[0].name == "mine_exterior");
-        SlimeAppearanceObject rmBits = rmBitsStructure.Element.Prefabs[0].DeepCopy();
-        rmBits.AttachedBones =
-        [
+        SlimeAppearanceStructure[] largoStructures = SRSingleton<GameContext>.Instance.SlimeDefinitions.GetSlimeByIdentifiableId(largoId).AppearancesDefault[0].Structures;
+        GameObject slime2object = SRSingleton<GameContext>.Instance.LookupDirector.GetIdentifiable(slime2).gameObject;
+        foreach (SlimeAppearanceStructure structure in largoStructures)
+        {
+            SlimeAppearanceObject structureCopy = structure.Element.Prefabs[0].DeepCopy();
+            structureCopy.AttachedBones =
+            [
             SlimeAppearance.SlimeBone.Slime,
             SlimeAppearance.SlimeBone.JiggleRight,
             SlimeAppearance.SlimeBone.JiggleLeft,
@@ -128,18 +97,55 @@ public static class LargoManager
             SlimeAppearance.SlimeBone.JiggleBottom,
             SlimeAppearance.SlimeBone.JiggleFront,
             SlimeAppearance.SlimeBone.JiggleBack
-        ];
-        SlimeAppearanceApplicator slimeApp = slimeRosi.GetComponent<SlimeAppearanceApplicator>();
-        var rootMatrix = slimeApp.Bones.First(x => x.Bone == SlimeAppearance.SlimeBone.Root).BoneObject.transform.localToWorldMatrix;
-        var poses = new Matrix4x4[rmBits.AttachedBones.Length];
-        for (var i = 0; i < rmBits.AttachedBones.Length; i++)
-        {
-            var bone = rmBits.AttachedBones[i];
-            poses[i] = slimeApp.Bones.First(x => x.Bone == bone).BoneObject.transform.worldToLocalMatrix * rootMatrix;
+            ];
+            SlimeAppearanceApplicator slimeApp = slime2object.GetComponent<SlimeAppearanceApplicator>();
+            var rootMatrix = slimeApp.Bones.First(x => x.Bone == SlimeAppearance.SlimeBone.Root).BoneObject.transform.localToWorldMatrix;
+            var poses = new Matrix4x4[structureCopy.AttachedBones.Length];
+            for (var i = 0; i < structureCopy.AttachedBones.Length; i++)
+            {
+                var bone = structureCopy.AttachedBones[i];
+                poses[i] = slimeApp.Bones.First(x => x.Bone == bone).BoneObject.transform.worldToLocalMatrix * rootMatrix;
+            }
+            for (int i = 0; i < structure.Element.Prefabs.Length; i++)
+            {
+                structure.Element.Prefabs[i] = structureCopy;
+            }
         }
-        for (int i = 0; i < rmBitsStructure.Element.Prefabs.Length; i++)
-        {
-            rmBitsStructure.Element.Prefabs[i] = rmBits;
-        }
+        //fix texture wrap on Rosi largos.
+    }
+    public static void CreateLargo(Identifiable.Id largoId, Identifiable.Id slime1, Identifiable.Id slime2, SlimeRegistry.LargoProps props, SlimeRegistry.LargoProps slime1SSProps = SlimeRegistry.LargoProps.NONE, SlimeRegistry.LargoProps slime2SSProps = SlimeRegistry.LargoProps.NONE, SlimeRegistry.LargoProps slime12SSProps = SlimeRegistry.LargoProps.NONE)
+    {
+        CreateLargo(largoId, slime1, slime2, props, out var _, out var _, out var _, slime1SSProps, slime2SSProps, slime12SSProps);
+    }
+    public static void LoadAllLargos()
+    {
+        //COCO LARGOS
+        /*SlimeRegistry.CraftLargo(Ids.PINK_COCO_LARGO, Ids.COCO_SLIME, IdentifiableId.PINK_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_SABER_LARGO, Ids.COCO_SLIME, IdentifiableId.SABER_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_QUANTUM_LARGO, Ids.COCO_SLIME, IdentifiableId.QUANTUM_SLIME, SlimeRegistry.LargoProps.REPLACE_BASE_MAT_AS_SLIME2);
+        SlimeRegistry.CraftLargo(Ids.COCO_HONEY_LARGO, Ids.COCO_SLIME, IdentifiableId.HONEY_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_PHOSPHOR_LARGO, Ids.COCO_SLIME, IdentifiableId.PHOSPHOR_SLIME,  SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_MOSAIC_LARGO, Ids.COCO_SLIME, IdentifiableId.MOSAIC_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_TANGLE_LARGO, Ids.COCO_SLIME, IdentifiableId.TANGLE_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_BOOM_LARGO, Ids.COCO_SLIME, IdentifiableId.BOOM_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_RAD_LARGO, Ids.COCO_SLIME, IdentifiableId.RAD_SLIME, SlimeRegistry.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_ROCK_LARGO, Ids.COCO_SLIME, IdentifiableId.ROCK_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_TABBY_LARGO, Ids.COCO_SLIME, IdentifiableId.TABBY_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_HUNTER_LARGO, Ids.COCO_SLIME, IdentifiableId.HUNTER_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_CRYSTAL_LARGO, Ids.COCO_SLIME, IdentifiableId.CRYSTAL_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_DERVISH_LARGO, Ids.COCO_SLIME, IdentifiableId.DERVISH_SLIME, SlimeRegistry.LargoProps.NONE);
+        SlimeRegistry.CraftLargo(Ids.COCO_MESMER_LARGO, Ids.COCO_SLIME, Ids.MESMER_SLIME, SlimeRegistry.LargoProps.NONE);*/
+        // SlimeRegistry.CraftLargo(Ids.COCO_HERMIT_LARGO, Ids.COCO_SLIME, Ids.HERMIT_SLIME, SlimeRegistry.LargoProps.NONE);
+
+        //MESMER LARGOS
+
+        //HERMIT LARGOS
+
+        //ROSI LARGOS
+        CreateLargo(Ids.ROSI_MINE_LARGO, Ids.ROSI_SLIME, Ids.MINE_SLIME, SlimeRegistry.LargoProps.REPLACE_BASE_MAT_AS_SLIME2 | SlimeRegistry.LargoProps.RECOLOR_SLIME1_ADDON_MATS);
+        CreateLargo(Ids.ROSI_LANTERN_LARGO, Ids.ROSI_SLIME, Ids.LANTERN_SLIME, SlimeRegistry.LargoProps.RECOLOR_BASE_MAT_AS_SLIME2 | SlimeRegistry.LargoProps.RECOLOR_SLIME1_ADDON_MATS | SlimeRegistry.LargoProps.SWAP_EYES);
+
+        //MINE LARGOS
+        CreateLargo(Ids.MINE_LANTERN_LARGO, Ids.MINE_SLIME, Ids.LANTERN_SLIME, SlimeRegistry.LargoProps.RECOLOR_SLIME2_ADDON_MATS | SlimeRegistry.LargoProps.SWAP_EYES);
     }
 }
