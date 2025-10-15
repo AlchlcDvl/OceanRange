5b41adb5a1e5b848c675f37af61e62c9302f4c22
AlchlcDvl <56699741+AlchlcDvl@users.noreply.github.com>
2025-10-14 22:59:24 -0400

    move files and delete unrelated ones

--- Bundle/Assets/OceanRangeScripts/JsonData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.IO;
-
-public abstract class JsonData : ScriptableObject
-{
-    public string Name;
-
-    public abstract void SerialiseTo(BinaryWriter writer);
-}
